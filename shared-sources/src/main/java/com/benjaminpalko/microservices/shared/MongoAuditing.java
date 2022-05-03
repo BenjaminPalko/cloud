0f76d52d526e8d5a9206ba6fedd2c2c355670ced
benjamin <benjaminpalko@hotmail.com>
2022-05-03 00:14:30 -0400

    MongoAuditing

---
+++ shared-sources/src/main/java/com/benjaminpalko/microservices/shared/MongoAuditing.java
@@ -0,0 +1,42 @@
+package com.benjaminpalko.microservices.shared;
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.util.Date;
+
+public abstract class MongoAuditing {
+    @CreatedDate
+    private final Date createdOn;
+    @LastModifiedDate
+    private final Date lastModifiedOn;
+    @CreatedBy
+    private final String createBy;
+    @LastModifiedBy
+    private final String lastModifiedBy;
+
+    protected MongoAuditing(Date createdOn, Date lastModifiedOn, String createBy, String lastModifiedBy) {
+        this.createdOn = createdOn;
+        this.lastModifiedOn = lastModifiedOn;
+        this.createBy = createBy;
+        this.lastModifiedBy = lastModifiedBy;
+    }
+
+    public Date getCreatedOn() {
+        return createdOn;
+    }
+
+    public Date getLastModifiedOn() {
+        return lastModifiedOn;
+    }
+
+    public String getCreateBy() {
+        return createBy;
+    }
+
+    public String getLastModifiedBy() {
+        return lastModifiedBy;
+    }
+}
