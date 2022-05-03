91933156d47abebb5055b5e694f1689276938e91
benjamin <benjaminpalko@hotmail.com>
2022-05-03 10:18:05 -0400

    import SharedQueueConfig

--- user-service/src/main/java/com/benjaminpalko/microservices/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/benjaminpalko/microservices/userservice/UserServiceApplication.java
@@ -1,8 +1,11 @@
 package com.benjaminpalko.microservices.userservice;
 
+import com.benjaminpalko.microservices.shared.queues.SharedQueueConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
+@Import(SharedQueueConfig.class)
 @SpringBootApplication
 public class UserServiceApplication {
 
