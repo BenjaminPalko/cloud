efc53c78a2d39d9422d442dbf75406071094767a
benjamin <benjaminpalko@hotmail.com>
2022-05-03 10:22:40 -0400

    SharedQueueConfig add userQueueName string bean

--- shared-sources/src/main/java/com/benjaminpalko/microservices/shared/queues/SharedQueueConfig.java
+++ shared-sources/src/main/java/com/benjaminpalko/microservices/shared/queues/SharedQueueConfig.java
@@ -1,14 +1,20 @@
 package com.benjaminpalko.microservices.shared.queues;
 
 import org.springframework.amqp.core.Queue;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SharedQueueConfig {
 
-    @Bean
-    public Queue userQueue() {
-        return new Queue("userQueue", false);
+    @Bean("userQueueName")
+    public String userQueueName() {
+        return "userQueue";
+    }
+
+    @Bean("userQueue")
+    public Queue userQueue(@Qualifier("userQueueName") String userQueueName) {
+        return new Queue(userQueueName, false);
     }
 }
