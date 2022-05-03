aae624ba03503485d1c3d39985a244233b892a96
benjamin <benjaminpalko@hotmail.com>
2022-05-03 00:18:21 -0400

    user details

--- shared-sources/src/main/java/com/benjaminpalko/microservices/shared/User.java
+++ shared-sources/src/main/java/com/benjaminpalko/microservices/shared/User.java
@@ -8,12 +8,30 @@ import java.util.UUID;
 public class User extends MongoAuditing {
     @Id
     private final UUID id;
-    protected User(Date createdOn, Date lastModifiedOn, String createBy, String lastModifiedBy, UUID id) {
+    private final String firstName;
+    private final String lastName;
+    private final Date birthday;
+    protected User(Date createdOn, Date lastModifiedOn, String createBy, String lastModifiedBy, UUID id, String firstName, String lastName, Date birthday) {
         super(createdOn, lastModifiedOn, createBy, lastModifiedBy);
         this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthday = birthday;
     }
 
     public UUID getId() {
         return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Date getBirthday() {
+        return birthday;
     }
 }
